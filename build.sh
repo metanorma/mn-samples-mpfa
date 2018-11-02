ceeeec8ec0c9d5fafb73f106e47d043e11786a52
Ronald Tse <ronald.tse@ribose.com>
2018-11-02 08:20:37 +0800

    Update gems and use recursive makefile

--- build.sh
+++
@@ -1,9 +0,0 @@
-cd bpn
-asciidoctor --trace -b mpfd -r 'metanorma-mpfd' raw.adoc
-cd ../compliance
-asciidoctor --trace -b mpfd -r 'metanorma-mpfd' raw.adoc
-cd ../guidelines
-asciidoctor --trace -b mpfd -r 'metanorma-mpfd' v2.adoc
-cd ../standards
-asciidoctor --trace -b mpfd -r 'metanorma-mpfd' raw.adoc
-cd ..
